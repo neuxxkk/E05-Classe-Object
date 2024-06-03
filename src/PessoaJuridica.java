fd31033668ae84b5486cf42889d0c09c67f1959d
= <=>
2024-06-03 09:23:39 -0300

    a

---
+++ src/PessoaJuridica.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class PessoaJuridica extends Cliente {
+
+    String cnpj;
+    int numFuncionarios;
+    String setor;
+
+    public PessoaJuridica(String nome,  String endereco, String cnpj, int numFuncionarios, String setor){
+         super(nome, endereco, new Date());
+         this.cnpj=cnpj;
+         this.numFuncionarios = numFuncionarios;
+         this.setor = setor;
+         
+    }
+
+    public String toString(){
+        return 
+        "\n===== Cliente PJ ====="+
+        "\nNome: " + this.nome+
+        "\nSetor: " + this.setor+
+        "\nNumero de funcionários: " + this.numFuncionarios+
+        "\nEndereco: " + this.endereco+
+        "\nCNPJ: " + this.cnpj+
+        "\n====================";
+    }
+
+}
